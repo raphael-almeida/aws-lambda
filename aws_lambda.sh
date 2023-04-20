2916460aeaea4a1c8726d7bce71764ca049bccc1
Raphael <raphael.augusto.almeida@gmail.com>
2023-04-20 18:11:29 -0300

    fix output file

--- aws_lambda.sh
+++ aws_lambda.sh
@@ -8,5 +8,5 @@ aws lambda create-function --function-name print-envs \
 --zip-file fileb://function_code.zip --handler index.handler --runtime nodejs12.x \
 --role arn:aws:iam::953029374461:role/lambda-execution-role
 
-aws lambda invoke --function-name print-envs out --log-type Tail \
+aws lambda invoke --function-name print-envs out.log --log-type Tail \
 --query 'LogResult' --output text | base64 -D
